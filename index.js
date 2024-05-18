5fd379647c59ffcf291e964688a954c7f2626560
André Diego Piske <andrepiske@gmail.com>
2024-05-18 11:13:39 -0300

    show host

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const process = require('node:process')
+const express = require('express');
+const process = require('node:process');
 
-const app = express()
+const app = express();
 
 process.on('SIGINT', () => {
   // console.log('Received SIGINT. Press Control-D to exit.')
-  process.exit()
-})
+  process.exit();
+});
 
-app.get('/', function (req, res) {
-  res.append('Cache-Control', 'no-store, no-cache')
-  res.append('Set-Cookie', 'exp-live=true; Path=/; Secure')
+app.get('/', (req, res) => {
+  res.append('Cache-Control', 'no-store, no-cache');
+  res.append('Set-Cookie', 'exp-live=true; Path=/; Secure');
 
-  var userName = 'anonymous'
+  const hostHeader = req.get('host');
+
+  var userName = 'anonymous';
   if (req.query.name) {
     userName = req.query.name
   }
 
-  let content = `<p>Hello World, you are <strong>${userName}</strong>!</p>`
-  content += "<p>Best viewed in 800x600</p>"
+  let content = `<p>Hello World, you are <strong>${userName}</strong>!</p>`;
+  content += "<p>Best viewed in 800x600</p>";
+  content += `<p>Requested host is <i>${hostHeader}</i></p>`;
 
-  res.send(content)
-})
+  res.send(content);
+});
 
-app.listen(3000)
+app.listen(3000);
